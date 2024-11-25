b8cc8d1d1f2d14c02e6077682f75bda32bd40ad6
fernando <422066442@pcpuma.acatlan.unam.mx>
2024-11-25 17:26:15 -0600

    Initial Commit

---
+++ aca_healt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  aca_healt
+//
+//  Created by CEDAM35 on 25/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
